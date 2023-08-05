6d1383e78d4b14cfbe6e68af864f9c47bb87208e
betulllll <betulsr0@gmail.com>
2023-08-05 20:27:11 +0300

    .

--- src/component/blog/BlogCreate.jsx
+++ src/component/blog/BlogCreate.jsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
+import { withTranslation } from 'react-i18next';
 
 export default class BlogCreate extends Component {
+    // Componentteki isim
+    static displayName = "Blog_Create";
+
+    // Constructor
+    constructor(props) {
+      super(props);
+  
+      // STATE
+      this.state = {
+        header: null,
+        content: null,
+        blogDto: {}, //object
+        isRead: false, // sözleşme kuralları
+      };
+     } //end constructor
+
   render() {
     return (
       <div>BlogCreate</div>
