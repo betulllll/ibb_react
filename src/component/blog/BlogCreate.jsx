444d9c7c9094bc9469616f81a191b9a46d7a8b23
betulllll <betulsr0@gmail.com>
2023-08-12 21:38:48 +0300

    spinner

--- src/component/blog/BlogCreate.jsx
+++ src/component/blog/BlogCreate.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { withTranslation } from 'react-i18next';
+import BlogApi from '../../services/BlogApi';
 
 class BlogCreate extends Component {
 
@@ -16,11 +17,13 @@ class BlogCreate extends Component {
       content: null,
       blogDto: {}, //object
       isRead: false, // sözleşme kuralları
+      spinnerData: false, //Spinner
     }; //end constructor
 
     // BIND
     this.onChangeIsRead = this.onChangeIsRead.bind(this);
     this.onChangeInputValue = this.onChangeInputValue.bind(this);
+    this.createSubmit = this.createSubmit.bind(this);
   }
 
   // CDM
@@ -40,16 +43,57 @@ class BlogCreate extends Component {
     // console.log("name:"+name);
     // const value=event.target.value;
     // console.log("value: "+value);
-
+    
     //2.YOL
     const { name, value } = event.target
     //console.log(name+" "+value);
-    //console.log(`${name} ${value}`);
+    console.log(`${name} ${value}`);
 
     // STATE
     this.setState({
       [name]: value,
     })
+  }
+
+  // CREATE SUBMIT
+  createSubmit = async (event) => {
+    // Browser sen dur bir şey yapma
+    event.preventDefault();
+    const { header, content } = this.state;
+    const blogDto = {
+      header, content
+    }
+    console.log(blogDto);
+
+    // 1 .YOL(promise)
+    /*
+    BlogApi.blogServiceCreate(blogDto).then((response) => {
+      if (response.status == 200) {
+        //alert("Ekleme Başarılı")
+        console.log("Ekleme Başarılı")
+      }
+    }).catch((err) => {
+      console.error(err)
+    }); */
+    //end then
+
+    // 2.YOL(asyn-await)
+    try {
+      // SPINNER GÖNDERMEDEN ÖNCE
+      this.setState({ spinnerData: true })
+      // CREATE
+      const response = await BlogApi.blogServiceCreate(blogDto);
+      if (response.status == 200) {
+        alert("Ekleme Başarılı")
+        //console.log("Ekleme Başarılı");
+        // SPINNER GÖNDERMEDEN ÖNCE
+        this.setState({ spinnerData: false })
+      }
+    } catch (err) {
+      console.error(err);
+      // HATA SPINNER ÇALIŞSIN
+      this.setState({ spinnerData: true })
+    }
   }
 
   //RENDER
@@ -99,16 +143,24 @@ class BlogCreate extends Component {
               onChange={this.onChangeIsRead}
               id="isReadId"
               name="isReadId" />
-            <label className="form-check-label" htmlFor="isReadId"> Anlaşmayı okudunuz mu </label>
+            <label className="form-check-label" htmlFor="isReadId"> Anlaşmayı okunuz mu </label>
           </div>
+
+          {/* RESET */}
+          <button
+            className="btn btn-danger mb-5 me-2">{t('reset')}
+          </button>
 
           {/* SUBMIT */}
           <button
-            className="btn btn-danger mb-5 me-2">{t('reset')}</button>
-
-          <button
+            type="submit"
             className="btn btn-primary mb-5"
-            disabled={!isRead}>{t('submit')}</button>
+            disabled={!isRead}
+            onClick={this.createSubmit}>
+            {(this.state.spinnerData) && <span className="spinner-border text-warning"></span>}
+            {t('submit')}
+          </button>
+
           <br /><br /><br /><br /> <br /><br /><br />
         </form>
       </React.Fragment>
